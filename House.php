d158cc7006616851b91e0e5bc9812ca6d59d3b1a
Enock-meston <ndagijimanaenock11@gmail.com>
2022-02-18 10:22:23 -0800

    i

--- House.php
+++ House.php
@@ -6,7 +6,20 @@
          
      }
  }
-
+ 
+ class House
+ {
+     public $primaryColor ="black";
+     public function changeColor(){
+         $this->primaryColor = $color;
+     }
+ }
+ 
  $example= new Example();
  $example->echo("Hell world");
+
+ $house = new House();
+ echo $house ->primaryColor;
+ $house->changeColor('white');
+
 ?>
